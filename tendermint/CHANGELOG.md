aa20d0536b459a3f9a79f3c569fb04ae305476df
Anton Kaliaev <anton.kalyaev@gmail.com>
2020-11-19 15:28:34 +0400

    add tendermint

---
+++ tendermint/CHANGELOG.md
@@ -0,0 +1,6 @@
+# Change Log
+All notable changes to this project will be documented in this file. This change log follows the conventions of [keepachangelog.com](http://keepachangelog.com/).
+
+## 0.2.0 - 2020-11-09
+### Changed
+- Upgraded deps (jepsen 0.2.1)
---
+++ tendermint/src/jepsen/tendermint/cli.clj
@@ -0,0 +1,27 @@
+(ns jepsen.tendermint.cli
+  "Command line interface to Tendermint tests."
+  (:require [clojure.pprint :refer [pprint]]
+            [clojure.tools.cli :as tc]
+            [jepsen [cli :as jc]]
+            [jepsen.tendermint [core :as core]]))
+
+(def opts
+  "Extra command line opts."
+  [[nil "--workload WORKLOAD" "Test workload to run; e.g. cas-register, set"
+    :default :cas-register
+    :parse-fn keyword]
+   [nil "--nemesis NEMESIS" "Nemesis to use; e.g. clocks"
+    :default :none
+    :parse-fn keyword]
+   [nil "--dup-validators" "Whether to have multiple validators share the same key."]
+   [nil "--super-byzantine-validators" "Should byzantine validators have just shy of 2/3 the voting weight?"]
+   (jc/package-opt "tendermint-url" "https://github.com/tendermint/tendermint/releases/download/v0.33.8/tendermint_v0.33.8_linux_amd64.zip")
+   (jc/package-opt "merkleeyes-url" "https://s3-us-west-2.amazonaws.com/tendermint/jepsen/merkleeyes_0.2.2-ngc.3_linux_amd64.zip")
+   (jc/package-opt "abci-url" "https://s3-us-west-2.amazonaws.com/tendermint/binaries/abci/v0.5.0/abci_0.5.0_linux_amd64.zip")])
+
+(defn -main
+  [& args]
+  (jc/run! (merge (jc/serve-cmd)
+                  (jc/single-test-cmd {:test-fn core/test
+                                       :opt-spec opts}))
+           args))
